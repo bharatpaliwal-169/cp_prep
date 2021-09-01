02494761248e37b488231583044a25168e89535a
Bharat Paliwal <18uec169@lnmiit.ac.in>
2021-09-01 17:36:26 +0530

    stack

---
+++ DSA/Stack/NLE.cpp
@@ -0,0 +1,46 @@
+//next larget element 
+#include <bits/stdc++.h>
+using namespace std;
+//ignore the duplicates in the input array.
+vector<int> nle(vector<int>& nums){
+  vector<int> ans;
+  stack<int> s;
+  for(int i = nums.size()-1; i >= 0;i--){
+    if(s.size() == 0){
+      ans.push_back(-1);
+    }
+    else if( (s.size() != 0||!s.empty()) && s.top() > nums[i]){
+      ans.push_back(s.top());
+    }
+    else if((s.size() != 0||!s.empty()) && s.top() < nums[i]){
+      while((s.size() != 0||!s.empty()) && s.top() < nums[i]){
+        s.pop();
+      }
+      if(s.empty()){
+        ans.push_back(-1);
+      }
+      else{
+        ans.push_back(s.top());
+      }
+    }
+    s.push(nums[i]);
+  }
+  reverse(ans.begin(), ans.end());
+  return ans;
+}
+
+
+int main(){
+  int n;cin >> n;
+  vector<int> a;
+  vector<int> res;
+  for(int i=0; i<n; i++){
+    int x;cin >> x;
+    a.push_back(x);
+  }
+  res = nle(a);
+  for(auto &i: res){
+    cout << i << " ";
+  }
+  return 0;
+}
